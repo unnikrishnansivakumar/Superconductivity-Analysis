56c091eae84ff430169854f2f978655fbc36e5b1
“Jinglun <jinglun.xue@mail.utoronto.ca>
2021-11-29 03:09:06 +0800

    Final Submission

--- a.R
+++ a.R
@@ -4,7 +4,7 @@
 
 supercon_data =read.csv('train.csv')
 
-smp_size <- floor(0.75 * nrow(supercon_data))
+smp_size <- floor(0.70 * nrow(supercon_data))
 
 ## set the seed to make your partition reproducible
 set.seed(10)
@@ -45,26 +45,19 @@ eval_results <- function(true, predicted, df) {
 
 library(glmnet)
 
-#perform k-fold cross-validation to find optimal lambda value
-cv_model <- cv.glmnet(X_train, y_train, alpha = 1,)
-
-#find optimal lambda value that minimizes test MSE
-best_lambda <- cv_model$lambda.min
-best_lambda
 
 
 
-#produce plot of test MSE by lambda value
 
 
 
-best_model <- glmnet(X_train, y_train, alpha = 1, lambda = best_lambda)
+best_model <- glmnet(X_train, y_train, alpha = 1, lambda = 0.5)
 
 
-predictions_train <- predict(best_model, s = best_lambda, newx = X_train)
+predictions_train <- predict(best_model, s = 0.5, newx = X_train)
 eval_results(y_train, predictions_train, train)
 
-ypr4 = predict(best_model, X_test, lambda = best_lambda)
+ypr4 = predict(best_model, X_test, lambda = 0.5)
 eval_results(y_test, ypr4, test)
 
 lasso_coef = coef(best_model)
@@ -73,19 +66,6 @@ sum(lasso_coef != 0)
 
 
 
-
-
-
-
-
-fit_ridge_cv= cv.glmnet(X_train, y_train, alpha = 0)# choosing best lambda using CV
-bestlam = fit_ridge_cv$lambda.min
-fit_ridge_best_cv = glmnet(X_train, y_train, alpha = 0, lambda = bestlam)
-
-ypr3 = predict(fit_ridge_best_cv, X_test, lambda = bestlam)
-eval_results(y_test, ypr3, test)
-
-lasso_coef = coef(fit_ridge_best_cv)
 
 
 
